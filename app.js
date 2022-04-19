dceff334f2234d94436af6d8fe356fb2d8e33ef5
teopyl <teopyl@gmail.com>
2022-04-19 13:34:23 +0300

    new mongoose methods for get and post data

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const { stringify } = require("nodemon/lib/utils");
 
 require("dotenv/config");
 const api = process.env.API_URL;
@@ -10,23 +11,49 @@ const api = process.env.API_URL;
 app.use(express.json()); //parse json data
 app.use(morgan("tiny")); //log http requests
 
-app.get(api + "/products", (req, res) => {
-  const product = {
-    id: 1,
-    name: "hair dresser",
-    image: "some_url",
-  };
-  res.send(product);
+//Schemas / Models
+const productSchema = mongoose.Schema({
+  name: String,
+  image: String,
+  countInStock: {
+    type: Number,
+    required: true
+  },
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+//alternative to handle promise with async and wait than .then and catch
+app.get(api + "/products", async (req, res) => {
+  const productList = await Product.find();
+
+  if (!productList)
+    res.status(500).json({ success: false });
+  res.send(productList);
 });
 
 app.post(api + "/products", (req, res) => {
-  const newProduct = req.body;
+  const newProduct = new Product({
+    name: req.body.name,
+    image: req.body.image,
+    countInStock: req.body.countInStock,
+  });
+
+  //save it in the Database
+  newProduct.save()
+    .then((newProduct) => {
+      res.status(201).json(newProduct);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+        success: false,
+      });
+    });
   console.log(newProduct);
-  res.send(newProduct);
 });
 
-mongoose
-  .connect(process.env.CONNECTION_STRING)
+mongoose.connect(process.env.CONNECTION_STRING)
   .then(() => {
     console.log("DataBase connection is ready");
   })
