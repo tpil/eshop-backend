34b8d333bc0eafc6c3573cf5407ced5ba4a0f804
teopyl <teopyl@gmail.com>
2022-04-19 18:04:11 +0300

    enable cors

--- app.js
+++ app.js
@@ -2,24 +2,30 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const cors = require('cors');
 
 require("dotenv/config");
-const api = process.env.API_URL;
-const categoriesRouter = require('./routers/categories');
-const productsRouter = require('./routers/products');
-const usersRouter = require('./routers/users');
-const ordersRouter = require('./routers/orders');
+
+//enable cors for all domain orgins
+app.use(cors());
+app.options('*', cors());
 
 //Midlewares
 app.use(express.json()); //parse json data
 app.use(morgan("tiny")); //log http requests
 
 //Routers - use routers as Midleware
+const api = process.env.API_URL;
+const categoriesRouter = require('./routers/categories');
+const productsRouter = require('./routers/products');
+const usersRouter = require('./routers/users');
+const ordersRouter = require('./routers/orders');
 app.use(api + '/categories', categoriesRouter);
 app.use(api + '/products', productsRouter);
 app.use(api + '/users', usersRouter);
 app.use(api + '/orders', ordersRouter);
 
+//Database
 mongoose.connect(process.env.CONNECTION_STRING)
   .then(() => {
     console.log("DataBase connection is ready");
